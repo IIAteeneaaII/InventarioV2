289d952494c5009429189b59b27ee268ac6b90c6
Steve <cosmeticaram78@gmail.com>
2025-07-28 10:41:59 -0600

    Cambios sweet alert
    sweet alert

---
+++ controllers/sweet-allert.js
@@ -0,0 +1,13 @@
+function showError(message) {
+  Swal.fire({
+    icon: 'error',
+    title: '¡Atención!',
+    text: message,
+    confirmButtonText: 'Entendido',
+    confirmButtonColor: '#d33',
+    background: '#fff',
+    color: '#333'
+  });
+}
+
+
