c2cf54e4c4926d2366ae215da51cb9af19d394b4
BzSaur <agomezj2101@alumno.ipn.mx>
2025-08-22 11:53:08 -0600

    Triggers funcionales de procesos

--- prisma/migrations/fix_userid_case.sql
+++
@@ -1,5 +0,0 @@
--- Script para corregir el problema de sincronización userid/userId
-ALTER TABLE "Registro" RENAME COLUMN "userId" TO "userId_temp";
-ALTER TABLE "Registro" ADD COLUMN "userId" INTEGER;
-UPDATE "Registro" SET "userId" = "userId_temp";
-ALTER TABLE "Registro" DROP COLUMN "userId_temp";
